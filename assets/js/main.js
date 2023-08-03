f6c703ed52a72bf4401156a36a30f8cbf7e83cde
Blake Hutchinson <blake.hutchinson.1996@gmail.com>
2023-08-02 22:30:04 -0500

    Detect browser language preferences, add redirect script

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,30 @@ function isYScrollable() {
     outlineScroll.style.overflowY = 'hidden';
   }
 }
+
+// Set timeout function for page redirect when splash video is loaded
+function videoIsLoaded() {
+  setTimeout(function() {
+    redirectByLang();
+  }, 25000);
+}
+
+function redirectByLang() {
+  if (navigator.language == "nb" || navigator.language == "nn" || navigator.language == "no") {
+    window.location.href = "./no/index.html";
+  } else {
+    window.location.href = "./en/index.html";
+  }
+}
+
+// Set volume control to 33%, 67%, 100%
+let volumePercent = document.getElementById("splashVideo");
+function vol33() {
+  volumePercent.volume = 0.33;
+}
+function vol67() {
+  volumePercent.volume = 0.67;
+}
+function vol100() {
+  volumePercent.volume = 1;
+}
